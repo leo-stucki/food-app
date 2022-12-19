be712989ecf2b949da5fb3d89cf7d59f83f5c9c9
e517550 <leo.stucki@sbb.ch>
2022-12-19 13:22:14 +0100

    main
    LS(FOODAPP):
     - styled patient view page

--- food-app/src/app/patient-view-page/patient-view-list/patient-view-list.component.ts
+++ food-app/src/app/patient-view-page/patient-view-list/patient-view-list.component.ts
@@ -1,36 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Patient} from "../../shared/model/patient";
+import {PatientService} from "../../shared/services/patient.service";
 
 @Component({
   selector: 'app-patient-view-list',
   templateUrl: './patient-view-list.component.html',
   styleUrls: ['./patient-view-list.component.scss']
 })
-export class PatientViewListComponent {
+export class PatientViewListComponent implements OnInit{
 
   columnsToDisplay = ['number', 'name', 'height', 'weight'];
 
-  patients: Patient[] = [
-    {
-      id: 1,
-      firstName: 'Lian',
-      lastName: 'Tschoggo',
-      weight: 100,
-      height: 1.80
-    },
-    {
-      id: 2,
-      firstName: 'livio',
-      lastName: 'spa',
-      weight: 100,
-      height: 1.80
-    },
-    {
-      id: 3,
-      firstName: 'linus',
-      lastName: 'Acker',
-      weight: 100,
-      height: 1.80
-    },
-  ];
+  patients: Patient[] = [];
+
+  constructor(private patientService: PatientService) {}
+
+  ngOnInit() {
+    this.patientService.getAllPatients().subscribe((res) => {
+      this.patients = res;
+    })
+  }
 }
